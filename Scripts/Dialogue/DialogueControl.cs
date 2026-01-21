97cbc56bd7139680dab8f3e3087b895ceb22ee62
Caio Varandas <169002267+caio-varandas@users.noreply.github.com>
2026-01-20 23:15:26 -0300

    Create DialogueControl.cs

---
+++ Scripts/Dialogue/DialogueControl.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueControl : MonoBehaviour
+{
+    [System.Serializable]
+    public enum idiom
+    {
+        pt,
+        eng,
+        spa
+    }
+
+    public idiom language;
+
+    [Header("Components")]
+    public GameObject dialogueObj;
+    public Image profileSprite;
+    public Text speechText;
+    public Text actorNameText;
+
+    [Header("Settings")]
+    public float typingSpeed;
+
+    private bool isShowing; //indica se o diálogo está ativo
+    private int index; //índice da frase atual
+    private string[] sentences;
+
+    //Singleton para acesso global ao DialogueControl
+    public static DialogueControl instance;
+
+    //inicializa o Singleton antes de qualquer Start()
+    public void Awake()
+    {
+        instance = this;
+    }
+
+    //exibe a frase atual com efeito de digitação
+    IEnumerator TypeSentence()
+    {
+        speechText.text = "";
+        foreach (char letter in sentences[index].ToCharArray())
+        {
+            speechText.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    public void NextSentence()
+    {
+        if(speechText.text == sentences[index])
+        {//frase atual terminou
+            if(index < sentences.Length - 1)
+            {
+                index++;
+                speechText.text = "";
+                StartCoroutine(TypeSentence());
+            }
+            else
+            {
+                speechText.text = "";
+                index = 0;
+                dialogueObj.SetActive(false);
+                sentences = null;
+                isShowing = false;
+            }
+        }
+    }
+
+    
+    public void Speech(string[] txt)
+    {
+        if (!isShowing)
+        {
+            dialogueObj.SetActive(true);
+            sentences = txt;
+            StartCoroutine(TypeSentence());
+            isShowing = true;
+        }
+    }
+}
