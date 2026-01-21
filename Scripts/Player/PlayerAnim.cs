416f077be6731d446484e3b91bb70c4045b261de
Caio Varandas <169002267+caio-varandas@users.noreply.github.com>
2026-01-20 23:31:45 -0300

    Create PlayerAnim.cs

---
+++ Scripts/Player/PlayerAnim.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//controla as animações do player com base no estado de movimento
+public class PlayerAnim : MonoBehaviour
+{
+    private Player player;
+    private Animator anim;
+    
+    void Start()
+    {
+        player = GetComponent<Player>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        OnMove();
+        OnRun();
+    }
+
+
+    #region Movement
+
+    //controla animações de movimento e rolagem
+    void OnMove()
+    {
+        if (player.direction.sqrMagnitude > 0)
+        {
+            if (player.isRolling)
+            {
+                anim.SetTrigger("isRoll");
+            }
+            else
+            {
+                anim.SetInteger("transition", 1);
+            }
+        }
+        else
+        {
+            anim.SetInteger("transition", 0);
+        }
+
+        //rotaciona o personagem conforme a direção horizontal
+        if (player.direction.x > 0)
+        {
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+
+        if (player.direction.x < 0)
+        {
+            transform.eulerAngles = new Vector2(0, 180);
+        }
+    }
+
+    //controla animação de corrida
+    void OnRun()
+    {
+        if (player.isRunning)
+        {
+            anim.SetInteger("transition", 2);
+        }
+    }
+
+    #endregion
+}
