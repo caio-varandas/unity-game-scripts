919d2991bcbf0e8fcdf14465e9403c33f5c200c1
Caio Varandas <169002267+caio-varandas@users.noreply.github.com>
2026-01-20 23:28:51 -0300

    Create Player.cs

---
+++ Scripts/Player/Player.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//controla o movimento e ações básicas do player
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float runSpeed;
+
+
+    private Rigidbody2D rig;
+    private float initialSpeed;
+    private bool _isRunning;
+    private bool _isRoolling;
+
+    //direção de movimento do player
+    private Vector2 _direction;
+
+    public Vector2 direction
+    { 
+        get { return _direction; }
+        set { _direction = value; }
+    }
+    public bool isRunning
+    {
+        get { return _isRunning; }
+        set { _isRunning = value; }
+    }
+    public bool isRolling
+    {
+        get { return _isRoolling; }
+        set { _isRoolling = value; }
+    }
+
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        initialSpeed = speed;
+    }
+
+
+    private void Update()
+    {
+        OnInput();
+        OnRun();
+        OnRolling();
+    }
+
+    private void FixedUpdate()
+    {
+        OnMove();
+    }
+
+
+
+    //organizar um bloco de codigo
+    #region Movement
+
+    //captura a direção de movimento do player
+    void OnInput()
+    {
+        _direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    //move o player respeitando a física
+    void OnMove()
+    {
+        rig.MovePosition(rig.position + _direction * speed * Time.fixedDeltaTime);
+    }
+
+    //alterna entre caminhada e corrida
+    void OnRun()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed = runSpeed;
+            _isRunning = true;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed = initialSpeed;
+            _isRunning = false;
+        }
+    }
+
+    //detecta a ação de rolagem
+    void OnRolling()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            _isRoolling = true;
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            _isRoolling = false;
+        }
+    }
+
+    #endregion
+}
