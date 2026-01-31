434bbdd8da1f9d4c614b49f05b57a18d5a615ead
Caio Varandas <169002267+caio-varandas@users.noreply.github.com>
2026-01-31 20:59:40 -0300

    Update PlayerItens.cs
    add resource system with limits and water handling

--- Scripts/Player/PlayerItens.cs
+++ Scripts/Player/PlayerItens.cs
@@ -4,18 +4,29 @@ using UnityEngine;
 
 public class PlayerItems : MonoBehaviour
 {
-    [SerializeField] private int totalWood;
+    [Header("Amounts")]
+    [SerializeField] private float totalWood;
+    [SerializeField] private float currentWater;
+    [SerializeField] private float totalCarrots;
 
-    public int TotalWood { get => totalWood; set => totalWood = value; }
+    [Header("Limits")]
+    private float waterLimit = 50;
+    private float carrotsLimit = 10;
+    private float woodLimit = 5;
 
+    public float TotalWood { get => totalWood; set => totalWood = value; }
+    public float CurrentWater { get => currentWater; set => currentWater = value; }
+    public float TotalCarrots { get => totalCarrots; set => totalCarrots = value; }
+    public float WaterLimit1 { get => waterLimit; set => waterLimit = value; }
+    public float CarrotsLimit { get => carrotsLimit; set => carrotsLimit = value; }
+    public float WoodLimit { get => woodLimit; set => woodLimit = value; }
 
-    void Start()
+    public void WaterLimit(float water)
     {
-        
+        if(currentWater <= WaterLimit1)
+        {
+            currentWater += water;
+        }
     }
 
-    void Update()
-    {
-        
-    }
 }
