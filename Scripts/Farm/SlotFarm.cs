3df08a846c5edecbbc162b3b5c8dfa2e6449d63a
Caio Varandas <169002267+caio-varandas@users.noreply.github.com>
2026-01-31 20:29:41 -0300

    Update SlotFarm.cs
    Add watering and carrot growth logic to farm slot

--- Scripts/Farm/SlotFarm.cs
+++ Scripts/Farm/SlotFarm.cs
@@ -4,18 +4,51 @@ using UnityEngine;
 
 public class SlotFarm : MonoBehaviour
 {
+    [Header("Components")]
     [SerializeField] private SpriteRenderer spriteRenderer;
     [SerializeField] private Sprite hole;
     [SerializeField] private Sprite carrot;
 
+    [Header("Settings")]
     [SerializeField] private int digAmount; //tempo para o player cavar para o buraco aparecer
+    [SerializeField] private float waterAmount; //quantidade de aágua para nascer uma cenoura
+
+    [SerializeField] private bool detecting;
+
     private int initialDigAmount;
+    private float currentWater;
+    private bool dugHole;
+
+    PlayerItems playerItems;
 
     void Start()
     {
+        playerItems = FindObjectOfType<PlayerItems>();
         initialDigAmount = digAmount;
     }
+    void Update()
+    {
+        if (dugHole)
+        {
+            if (detecting)
+            {
+                currentWater += 0.01f;
+            }
 
+            if(currentWater >= waterAmount) //encheu total de água necessaria
+            {
+                spriteRenderer.sprite = carrot;
+
+                if(Input.GetKeyDown(KeyCode.E))
+                {
+                    spriteRenderer.sprite = hole;
+                    playerItems.TotalCarrots++;
+                    currentWater = 0f;
+                }
+            }
+        }
+        
+    }
     public void OnHit()
     {
         digAmount--;
@@ -23,18 +56,26 @@ public class SlotFarm : MonoBehaviour
         if (digAmount <= initialDigAmount / 2) //cria o buraco
         { 
             spriteRenderer.sprite = hole;
+            dugHole = true;
         }
-
-        /*if (digAmount <= 0) //então plantar cenoura
-        {
-            spriteRenderer.sprite = carrot;
-        }*/
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Dig"))
         {
             OnHit();
+        }
+
+        if (collision.CompareTag("Water"))
+        {
+            detecting = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Water"))
+        {
+            detecting = false;
         }
     }
 }
