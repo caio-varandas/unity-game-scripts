6ff60cd459c6ca9348c9eaf990d740cec21caf9e
Caio Varandas <169002267+caio-varandas@users.noreply.github.com>
2026-01-31 20:32:11 -0300

    Create HUDController.cs

---
+++ Scripts/HUD/HUDController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDController : MonoBehaviour
+{
+    [Header("Items")]
+    [SerializeField] private Image waterUIBar;
+    [SerializeField] private Image woodUIBar;
+    [SerializeField] private Image carrotUIBar;
+
+    [Header("Tools")]
+    //[SerializeField] private Image axeUI;
+    //[SerializeField] private Image shovelUI;
+    //[SerializeField] private Image bucketUI;
+    public List<Image> toolsUI = new List<Image>();
+    [SerializeField] private Color selectColor;
+    [SerializeField] private Color alphaColor;
+
+    private PlayerItems playerItems;
+    private Player player;
+
+    private void Awake()
+    {
+        playerItems = FindAnyObjectByType<PlayerItems>();
+        player = playerItems.GetComponent<Player>(); //so para puxar o objeto ja que estão no mesmo objeto
+    }
+
+
+    void Start()
+    {
+        waterUIBar.fillAmount = 0f;
+        woodUIBar.fillAmount = 0f;
+        carrotUIBar.fillAmount = 0f;
+    }
+
+    void Update()
+    {
+        waterUIBar.fillAmount = playerItems.CurrentWater / playerItems.WaterLimit1;
+        woodUIBar.fillAmount = playerItems.TotalWood / playerItems.WoodLimit;
+        carrotUIBar.fillAmount = playerItems.TotalCarrots / playerItems.CarrotsLimit;
+
+        for(int i = 0; i < toolsUI.Count; i++)
+        {
+            toolsUI[i].color = alphaColor;
+        }
+        toolsUI[player.HandlingObj].color = selectColor;
+    }
+}
